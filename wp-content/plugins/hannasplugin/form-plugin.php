64c3e6a059b29fc683ac59cb0d5030875f6712b8
Hannane <hannanekabuli@gmail.com>
2022-04-27 16:23:15 +0100

    nya image

--- wp-content/plugins/hannasplugin/form-plugin.php
+++ wp-content/plugins/hannasplugin/form-plugin.php
@@ -3,8 +3,18 @@
 // Description: Ett testplugin
 
 
+add_action('wp_enqueue_scripts', 'min_script_laddare');
+
+function min_script_laddare(){
+
+    wp_enqueue_script('jquery');
+}
+
+
+
+
 add_action('wp_ajax_skicka-form','skriv_ut_banan');
-//add_action('wp_ajax_skicka-form','skriv_ut_banan');
+add_action('wp_ajax_nopriv_skicka-form','skriv_ut_banan');
 
 function skriv_ut_banan(){
 
